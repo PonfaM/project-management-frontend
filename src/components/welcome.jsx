371c0ae48861d3a4a3938f78b03ee150ab631914
PonfaM <ponfamichaeljosep@gmail.com>
2026-08-31 11:32:33 -0700

    Project Management FrontEnd

---
+++ src/components/welcome.jsx
@@ -0,0 +1,13 @@
+
+function welcome() { 
+
+    return (
+        <div>
+            <h1>Welcome to the App</h1>
+            <p>This is a simple welcome message.</p>
+        </div>
+    )
+
+  }
+
+  export default welcome;
